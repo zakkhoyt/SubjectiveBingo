97fa2c35f744731a4a7ec10a89f0eaf67d24549a
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-12-07 23:43:58 -0800

    Fillingout GK stuff

--- SubjectiveBingo/ZHGameKitHelper.swift
+++ SubjectiveBingo/ZHGameKitHelper.swift
@@ -5,12 +5,23 @@
 //  Created by Zakk Hoyt on 12/7/15.
 //  Copyright © 2015 Zakk Hoyt. All rights reserved.
 //
+//  See: http://www.raywenderlich.com/60998/game-center-tutorial-how-to-make-a-simple-multiplayer-game-with-sprite-kit-part-2
+
+
 
 import UIKit
 import GameKit
 
 let ZHGameKitHelperLocalPlayerAuthenticated = "ZHGameKitHelperLocalPlayerAuthenticated"
 let ZHGameKitHelperPresentAuthenticationViewController = "ZHGameKitHelperPresentAuthenticationViewController"
+
+
+protocol ZHGameKitHelperDelegate {
+    func matchStarted()
+    func matchEnded()
+    func match(match: GKMatch, data: NSData, playerID: String)
+}
+
 
 class ZHGameKitHelper: NSObject {
 
@@ -30,6 +41,9 @@ class ZHGameKitHelper: NSObject {
     }
     
     var enabledGameCenter: Bool = false
+    var matchStarted: Bool = false
+    var match: GKMatch? = nil
+    var delegate: ZHGameKitHelperDelegate? = nil
     
     func authenticatePlayer() {
         let localPlayer = GKLocalPlayer.localPlayer()
@@ -55,5 +69,42 @@ class ZHGameKitHelper: NSObject {
         })
     }
     
+    
+    func findMatch(minPlayers: Int, maxPlayers: Int, viewController: UIViewController, delegate: ZHGameKitHelperDelegate) {
+        if !enabledGameCenter {
+            return
+        }
+        
+        self.matchStarted = false
+        self.match = nil
+        self.delegate = delegate
+        viewController.dismissViewControllerAnimated(true, completion: nil)
+        
+        let request = GKMatchRequest()
+        request.minPlayers = minPlayers
+        request.maxPlayers = maxPlayers
+        
+        let mmvc = GKMatchmakerViewController(matchRequest: request)
+        mmvc?.matchmakerDelegate = self
+        viewController.presentViewController(mmvc!, animated: true, completion: nil)
+    }
 }
 
+extension ZHGameKitHelper: GKMatchmakerViewControllerDelegate {
+    func matchmakerViewControllerWasCancelled(viewController: GKMatchmakerViewController) {
+        
+    }
+    
+    func matchmakerViewController(viewController: GKMatchmakerViewController, didFailWithError error: NSError) {
+        
+    }
+    
+    func matchmakerViewController(viewController: GKMatchmakerViewController, didFindMatch match: GKMatch) {
+        
+    }
+    
+}
+
+extension ZHGameKitHelper: GKMatchDelegate {
+    
+}
