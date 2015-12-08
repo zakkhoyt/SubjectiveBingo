c6af6dcb1c2eeddb196963ca58261e4cf781bb31
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-12-07 23:31:18 -0800

    first commit

--- SubjectiveBingo/ZHGameKitHelper.swift
+++ SubjectiveBingo/ZHGameKitHelper.swift
@@ -6,8 +6,54 @@
 //  Copyright © 2015 Zakk Hoyt. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+import GameKit
+
+let ZHGameKitHelperLocalPlayerAuthenticated = "ZHGameKitHelperLocalPlayerAuthenticated"
+let ZHGameKitHelperPresentAuthenticationViewController = "ZHGameKitHelperPresentAuthenticationViewController"
 
 class ZHGameKitHelper: NSObject {
 
+    
+    var lastError: NSError? = nil {
+        didSet{
+            if let lastError = lastError {
+                print("Error: " + lastError.localizedDescription)
+            }
+        }
+    }
+    
+    var authenticationViewController: UIViewController? = nil {
+        didSet {
+            NSNotificationCenter.defaultCenter().postNotificationName(ZHGameKitHelperPresentAuthenticationViewController, object: nil)
+        }
+    }
+    
+    var enabledGameCenter: Bool = false
+    
+    func authenticatePlayer() {
+        let localPlayer = GKLocalPlayer.localPlayer()
+        
+        if localPlayer.authenticated {
+            NSNotificationCenter.defaultCenter().postNotificationName(ZHGameKitHelperLocalPlayerAuthenticated, object: nil)
+            return
+        }
+
+        localPlayer.authenticateHandler =  ({ (viewController: UIViewController?, error: NSError?) -> Void in
+
+            self.lastError = error
+
+            if let viewController = viewController {
+                self.authenticationViewController = viewController
+            } else if localPlayer.authenticated {
+                self.enabledGameCenter = true
+                NSNotificationCenter.defaultCenter().postNotificationName(ZHGameKitHelperLocalPlayerAuthenticated, object: nil)
+            } else {
+                self.enabledGameCenter = false
+            }
+            
+        })
+    }
+    
 }
+
--- SubjectiveBingo/ZHTilesTableViewController.swift
+++ SubjectiveBingo/ZHTilesTableViewController.swift
@@ -6,8 +6,51 @@
 //  Copyright © 2015 Zakk Hoyt. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class ZHTilesTableViewController: UITableViewController {
 
+    let tiles = ["Tile 1",
+        "Tile 2",
+        "Tile 3",
+        "Tile 4",
+        "Tile 5",
+        "Tile 6",
+        "Tile 7",
+        "Tile 8",
+        "Tile 9",
+        "Tile 10",
+        "Tile 11",
+        "Tile 12",
+        "Tile 13",
+        "Tile 14",
+        "Tile 15",
+        "Tile 16",
+        "Tile 17",
+        "Tile 18",
+        "Tile 19",
+        "Tile 20",
+        "Tile 21",
+        "Tile 22",
+        "Tile 23",
+        "Tile 24",
+        "Tile 25",
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let gameKitHelper = ZHGameKitHelper()
+        gameKitHelper.authenticatePlayer()
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 25
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        cell?.textLabel!.text = "\(indexPath.row + 1)"
+        cell?.detailTextLabel!.text = tiles[indexPath.row]
+        return cell!
+    }
 }
